ab440b3a65ac20f284e1f158f40cc9c807604424
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-17 18:35:57 +0300

    first add VendingMachine.java

--- OopHw1/Ex1/VendingMachine.java
+++ OopHw1/Ex1/VendingMachine.java
@@ -1,5 +1,47 @@
 package OopHw1.Ex1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
-    
+    List<Product> list = new ArrayList<>();
+
+    public int getAmount() {
+        return amount;
+    }
+
+    private int amount = 0;
+    public VendingMachine addProduct(Product product){
+        list.add(product);
+        return this;
+
+    }
+
+    public List<Product> getList() {
+        return list;
+    }
+
+    public Product findProduct(String string){
+        for (int i = 0; i < getList().size(); i++) {
+            if (getList().get(i).getName().equals(string)){
+                return getList().get(i);
+            }
+        }
+        return null;
+    }
+
+    public Product sellProduct(Product product){
+        try{
+            Product prodFound = findProduct(product.getName());
+            getList().remove(prodFound);
+            amount += prodFound.getCost();
+            return prodFound;
+
+        } catch(Exception ex) {
+            throw new RuntimeException("Product not found");
+
+        }
+
+
+    }
 }
