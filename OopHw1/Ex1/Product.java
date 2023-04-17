ed3473436dad3c5845779c607ef86e7f5a08a3e7
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-17 18:42:34 +0300

    first add Product.java

--- OopHw1/Ex1/Product.java
+++ OopHw1/Ex1/Product.java
@@ -1,5 +1,31 @@
 package OopHw1.Ex1;
 
 public class Product {
-    
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    private String name;
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    private Integer cost;
+
+
+    public Product(String name, Integer cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %d",name, cost);
+
+    }
 }
