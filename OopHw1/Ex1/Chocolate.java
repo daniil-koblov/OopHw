8298bac9653f3e34f9ea139cebc1c07fab43cd10
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-18 14:52:38 +0300

    first add Chocolate.java

--- OopHw1/Ex1/Chocolate.java
+++ OopHw1/Ex1/Chocolate.java
@@ -1,5 +1,17 @@
 package OopHw1.Ex1;
 
-public class Chocolate {
+public class Chocolate extends Product {
+    private final String type;
+
+    public Chocolate(String name, Integer cost, String type){
+        super(name, cost);
+
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ ", " + type;
+    }
     
 }
