403e330b5f8a108002ad64b57730647967660ff4
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:23:11 +0300

    first add Herbivores.java

--- OopHw2/Ex1/Herbivores.java
+++ OopHw2/Ex1/Herbivores.java
@@ -1,5 +1,10 @@
 package OopHw2.Ex1;
 
-public class Herbivores {
-    
+public abstract class Herbivores extends Animal{
+    public Herbivores(String name) {
+        super(name);
+    }
+    public String feed(){
+        return "grass";
+    }
 }
