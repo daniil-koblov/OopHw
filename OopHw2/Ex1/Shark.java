b5be5008c4f3d02b24e1b3e6e91a4f64307a24c1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:44:03 +0300

    first Shark.java

--- OopHw2/Ex1/Shark.java
+++ OopHw2/Ex1/Shark.java
@@ -1,5 +1,17 @@
 package OopHw2.Ex1;
 
-public class Shark {
-    
+public class Shark extends Predator implements Swimable {
+    public Shark(String name){
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "<Jaws are chattering nearby>";
+    }
+
+    @Override
+    public int speedForSwim() {
+        return 60;
+    }
 }
