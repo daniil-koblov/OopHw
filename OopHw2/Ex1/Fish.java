b20a41d459b24b2990671edda6c57312e1d17a01
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:55:56 +0300

    first add Fish.java

--- OopHw2/Ex1/Fish.java
+++ OopHw2/Ex1/Fish.java
@@ -1,5 +1,22 @@
 package OopHw2.Ex1;
 
-public class Fish {
-    
+public class Fish extends Herbivores implements Swimable {
+    public Fish(String name) {
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "Pi-pi-pi";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Fish, %s", super.toString());
+    }
+
+    @Override
+    public int speedForSwim() {
+        return 25;
+    }
 }
