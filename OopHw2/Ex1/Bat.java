646c84c1b98c23c7b73182db782c258701d4ba3c
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:21:14 +0300

    first add Bat.java

--- OopHw2/Ex1/Bat.java
+++ OopHw2/Ex1/Bat.java
@@ -1,5 +1,19 @@
 package OopHw2.Ex1;
 
-public class Bat {
-    
+public class Bat extends Predator implements Flyable{
+
+    public Bat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "Pip-pip-pip";
+    }
+
+    @Override
+    public int speedForFly() {
+        return 120;
+    }
+
 }
