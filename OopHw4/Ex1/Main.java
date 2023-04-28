1bcafd78a392610625445a44b4abbb4e5a953af6
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:03:57 +0300

    first add Main.java

--- OopHw4/Ex1/Main.java
+++ OopHw4/Ex1/Main.java
@@ -1,5 +1,23 @@
 package OopHw4.Ex1;
 
 public class Main {
+    public static void main(String[] args) {
+        Team<Archer> archersTeam = new Team<>();
+        archersTeam.add(new Archer("George", new Bow(), 30))
+                    .add(new Archer("Bob", new Bow(), 25));
+        
+        Team<Assasin> assasinTeam = new Team<>();
+        assasinTeam.add(new Assasin("Ecio",new Knife(), 20))
+                    .add(new Assasin("Maria", new Knife(), 35));
+
+        System.out.println(archersTeam);
+        System.out.println(assasinTeam);
+        
+        Battle placeOfBattle = new Battle(new Archer("Henry", new Bow(), 30), 
+                               new Assasin("Jonny", new Knife(), 34));
+        Warrior winner = placeOfBattle.fight();
+
+        System.out.println(winner);
+    }
     
 }
