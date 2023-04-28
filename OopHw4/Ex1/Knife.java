3f619b13b4d8f6d1c52771733ad3eabda9e3b180
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:01:57 +0300

    first add Knife.java

--- OopHw4/Ex1/Knife.java
+++ OopHw4/Ex1/Knife.java
@@ -1,5 +1,13 @@
 package OopHw4.Ex1;
 
-public class Knife {
-    
+public class Knife implements Weapon {
+    @Override
+    public int damage() {
+        return 15;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Knife, max damage %d", damage());
+    }
 }
