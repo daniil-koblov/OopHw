3f63619cfbc823430b2539453837f797fbae13d6
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 18:49:54 +0300

    first add Archer.java

--- OopHw4/Ex1/Archer.java
+++ OopHw4/Ex1/Archer.java
@@ -1,6 +1,18 @@
 package OopHw4.Ex1;
 
-public class Archer {
+import java.util.Random;
 
-    
+public class Archer extends Warrior<Bow>{
+    public Archer(String name, Bow bow, int healthPoint) {
+        super(name, bow, healthPoint);
+    }
+    int range(){
+        Random random = new Random();
+        return random.nextInt(weapon.range());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Archer %s", super.toString());
+    }
 }
