a9a6574b53313dabd5cd98dd8ab63a41b7cc01a1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 18:50:49 +0300

    first add Assasin.java

--- OopHw4/Ex1/Assassin.java
+++ OopHw4/Ex1/Assassin.java
@@ -1,5 +1,13 @@
 package OopHw4.Ex1;
 
-public class Assassin {
+public class Assassin extends Warrior<Knife>{
+
+    public Assassin(String name, Knife knife, int healthPoint) {
+        super(name, knife, healthPoint);
+    }
     
+    @Override
+    public String toString() {
+        return String.format("Assassin %s", super.toString());
+    }
 }
