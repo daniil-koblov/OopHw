8c1a1b7aa5f384aa5e62b3f6f10082d323f3c22a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:45:04 +0300

    extends Shield Warrior.java

--- OopHw4/Ex1/Warrior.java
+++ OopHw4/Ex1/Warrior.java
@@ -2,7 +2,7 @@ package OopHw4.Ex1;
 
 import java.util.Random;
 
-public abstract class Warrior<T extends Weapon> {
+public abstract class Warrior<T extends Weapon, S extends Shield> {
     private String name;
 
     protected T weapon;
@@ -15,6 +15,16 @@ public abstract class Warrior<T extends Weapon> {
         this.weapon = weapon;
     }
 
+    protected S shield;
+
+    public S getShield() {
+        return shield;
+    }
+
+    public void setShield(S shield) {
+        this.shield = shield;
+    }
+
     private int healthPoint;
 
     public int getHealthPoint() {
@@ -25,19 +35,25 @@ public abstract class Warrior<T extends Weapon> {
         this.healthPoint = healthPoint;
     }
 
-    public Warrior(String name, T weapon, int healthPoint) {
+    public Warrior(String name, T weapon, T Shield, int healthPoint) {
         this.name = name;
         this.weapon = weapon;
+        this.shield = shield;
         this.healthPoint = healthPoint;
     }
 
     @Override
     public String toString() {
-        return String.format("Name %s, weapon %s, healthPoint %d", name, weapon, healthPoint);
+        return String.format("Name %s, weapon %s, shield %s, healthPoint %d", name, weapon, shield, healthPoint);
     }
 
     public int hit() {
         Random random = new Random();
         return random.nextInt(weapon.damage());
+    }
+
+    public int evasion(){
+        Random random = new Random();
+        return random.nextInt(shield.shieldProtection());
     }
 }
