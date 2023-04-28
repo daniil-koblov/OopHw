465aa17b1580aa81ad86a84104ec20a4c140f09a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:29:46 +0300

    set the parameter for the Spanish shield

--- OopHw4/Ex1/SpanishShield.java
+++ OopHw4/Ex1/SpanishShield.java
@@ -1,5 +1,13 @@
 package OopHw4.Ex1;
 
-public class SpanishShield {
-    
+public class SpanishShield implements Shield{
+    @Override
+    public int shieldProtection(){
+        return 7;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Spanish shield, max protection %d", shieldProtection());
+    }
 }
