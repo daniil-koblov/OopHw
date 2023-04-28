0fbf624273af6011b5b66025bfb702079c0831ec
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-29 00:03:04 +0300

    add no shield

--- OopHw4/Ex1/NoShield.java
+++ OopHw4/Ex1/NoShield.java
@@ -1,5 +1,13 @@
 package OopHw4.Ex1;
 
-public class NoShield {
-    
+public class NoShield implements Shield{
+    @Override
+    public int shieldProtection() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("No shield, max protection %d", shieldProtection());
+    }
 }
