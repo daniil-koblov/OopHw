318cfb8a0c47f2b7f60a0cfd40f16ac66db5abde
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:26:09 +0300

    set the parameter for the English shield

--- OopHw4/Ex1/EnglishShield.java
+++ OopHw4/Ex1/EnglishShield.java
@@ -1,5 +1,13 @@
 package OopHw4.Ex1;
 
-public class EnglishShield {
-    
+public class EnglishShield implements Shield{
+    @Override
+    public int shieldProtection(){
+        return 3;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("English shield, max protection %d", shieldProtection());
+    }
 }
