895ac8aae53427086b19af61dcded6e9a0582ab3
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 23:40:17 +0300

    add damage, range and toString in Katana.java

--- OopHw4/Ex1/Katana.java
+++ OopHw4/Ex1/Katana.java
@@ -1,5 +1,17 @@
 package OopHw4.Ex1;
 
-public class Katana {
-    
+public class Katana implements Weapon{
+    @Override
+    public int damage() {
+        return 15;
+    }
+
+    public int range(){
+        return 2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Knife, max damage %d, max range %d", damage(), range());
+    }
 }
