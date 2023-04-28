4dd6a3a4a6a1e2f71dcf5966b265423dc6f046d1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:05:55 +0300

    second add Main.java

--- OopHw4/Ex1/Main.java
+++ OopHw4/Ex1/Main.java
@@ -6,15 +6,15 @@ public class Main {
         archersTeam.add(new Archer("George", new Bow(), 30))
                     .add(new Archer("Bob", new Bow(), 25));
         
-        Team<Assasin> assasinTeam = new Team<>();
-        assasinTeam.add(new Assasin("Ecio",new Knife(), 20))
-                    .add(new Assasin("Maria", new Knife(), 35));
+        Team<Assassin> assasinTeam = new Team<>();
+        assasinTeam.add(new Assassin("Ecio",new Knife(), 20))
+                    .add(new Assassin("Maria", new Knife(), 35));
 
         System.out.println(archersTeam);
         System.out.println(assasinTeam);
         
         Battle placeOfBattle = new Battle(new Archer("Henry", new Bow(), 30), 
-                               new Assasin("Jonny", new Knife(), 34));
+                               new Assassin("Jonny", new Knife(), 34));
         Warrior winner = placeOfBattle.fight();
 
         System.out.println(winner);
