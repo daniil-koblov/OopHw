9781e4323a61d393ada276bfb8ce7b88ee573495
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-28 19:04:47 +0300

    first add Warrior.java

--- OopHw4/Ex1/Warrior.java
+++ OopHw4/Ex1/Warrior.java
@@ -1,5 +1,43 @@
 package OopHw4.Ex1;
 
-public class Warrior {
-    
+import java.util.Random;
+
+public abstract class Warrior<T extends Weapon> {
+    private String name;
+
+    protected T weapon;
+
+    public T getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(T weapon) {
+        this.weapon = weapon;
+    }
+
+    private int healthPoint;
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(int healthPoint) {
+        this.healthPoint = healthPoint;
+    }
+
+    public Warrior(String name, T weapon, int healthPoint) {
+        this.name = name;
+        this.weapon = weapon;
+        this.healthPoint = healthPoint;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name %s, weapon %s, healthPoint %d", name, weapon, healthPoint);
+    }
+
+    public int hit() {
+        Random random = new Random();
+        return random.nextInt(weapon.damage());
+    }
 }
