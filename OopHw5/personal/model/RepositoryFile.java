7882c1115f83d4574f46737f38f3e89e8d12308b
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-05-19 17:18:10 +0300

    second add RepositoryFile.java

--- OopHw5/personal/model/RepositoryFile.java
+++ OopHw5/personal/model/RepositoryFile.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RepositoryFile implements Repository {
-    private UserMapper mapper = new UserMapper();
+    private NewUserMapper mapper = new NewUserMapper();
     private FileOperation fileOperation;
 
     public RepositoryFile(FileOperation fileOperation) {
