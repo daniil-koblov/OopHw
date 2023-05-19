55e5ff223dcd38b5c3730e4d96a34e023d48d58e
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-05-19 17:22:44 +0300

    second add Main.java

--- OopHw5/personal/Main.java
+++ OopHw5/personal/Main.java
@@ -2,14 +2,18 @@ package OopHw5.personal;
 
 import OopHw5.personal.controllers.UserController;
 import OopHw5.personal.model.FileOperation;
-import OopHw5.personal.model.FileOperationImpl;
+import OopHw5.personal.model.FileOperationImplMd;
 import OopHw5.personal.model.Repository;
 import OopHw5.personal.model.RepositoryFile;
 import OopHw5.personal.views.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
-        FileOperation fileOperation = new FileOperationImpl("users.txt");
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("/oop_java/homeworks/HW05/src/personal/users.md");
+        System.out.println(pathFile);
+//        FileOperation fileOperation = new FileOperationImpl("users.txt");
+        FileOperation fileOperation = new FileOperationImplMd(pathFile);
         Repository repository = new RepositoryFile(fileOperation);
         UserController controller = new UserController(repository);
         ViewUser view = new ViewUser(controller);
