784e0a0cf7955bf1ccde3f2f3825d0920378145d
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-05-19 17:14:53 +0300

    first add NewUserMapper.java

--- OopHw5/personal/model/NewUserMapper.java
+++ OopHw5/personal/model/NewUserMapper.java
@@ -1,5 +1,14 @@
 package OopHw5.personal.model;
 
-public class NewUserMapper {
-    
+public class NewUserMapper extends UserMapper{
+    public String map(User user, String delimiter) {
+        String text = String.join(delimiter, user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+        return text;
+    }
+
+
+    public User map(String line, String delimiter) {
+        String[] lines = line.split(delimiter);
+        return new User(lines[0], lines[1], lines[2], lines[3]);
+    }
 }
